9895f71865d2787303705fe0838d6cf53de1c2bd
AntonioEspindolaAguirre <3520110524@uth.edu.mx>
2024-07-13 22:54:35 -0600

    admind list

--- PotaxieSport/Controllers/AdministradorController.cs
+++ PotaxieSport/Controllers/AdministradorController.cs
@@ -30,6 +30,33 @@ namespace PotaxieSport.Controllers
             return View();
         }
 
+        public IActionResult Administrador  ()
+        {
+            var administrador = _generalServicio.ObtenerUsuarios().Where(u => u.RolId == 2).ToList();
+            return View("~/Views/Administrador/Administrador/index.cshtml", administrador);
+        }
+        public IActionResult Coach()
+        {
+            var coach = _generalServicio.ObtenerUsuarios().Where(u => u.RolId == 5).ToList();
+            return View("~/Views/Administrador/Coach/index.cshtml", coach);
+        }
+        public IActionResult Arbitro()
+        {
+            var arbitro = _generalServicio.ObtenerUsuarios().Where(u => u.RolId == 4).ToList();
+            return View("~/Views/Administrador/Arbitro/index.cshtml", arbitro);
+        }
+        public IActionResult Contador()
+        {
+            var contador = _generalServicio.ObtenerUsuarios().Where(u => u.RolId == 3).ToList();
+            return View("~/Views/Administrador/Contador/index.cshtml", contador);
+        }
+        public IActionResult Doctor ()
+        {
+            var doctor = _generalServicio.ObtenerUsuarios().Where(u => u.RolId == 1).ToList();
+            return View("~/Views/Administrador/Doctor/index.cshtml", doctor);
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
