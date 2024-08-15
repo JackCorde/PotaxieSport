dc5885cf3119819e01abab28897f559b3d1134ee
AntonioEspindolaAguirre <3520110524@uth.edu.mx>
2024-08-14 21:19:32 -0600

    V

---
+++ PotaxieSport/Models/PartidoParticipacionDTO.cs
@@ -0,0 +1,13 @@
+﻿using PotaxieSport.Models.ViewModels;
+
+namespace PotaxieSport.Models
+{
+    public class PartidoParticipacionDTO
+    {
+        public Torneo torneo { get; set; }
+        public List<Equipos> equipos { get; set; }
+        public List<Partido> partidos { get; set; }
+        public List<Participacion> participacions { get; set; }
+    }
+
+}
